e4cb6746733c59961b18ab273e33857f849fc74c
VEDA0013391 <sayamina.56@gmail.com>
2026-07-16 15:07:08 +0900

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,6 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon.ico" },
       { url: "/site-icon.png", type: "image/png" },
     ],
     apple: "/site-icon.png",
